0e4e2a2f86a3b7995276c67c9d95e1e3534fd6ca
Neil <Neil10241126@gmail.com>
2023-12-09 14:54:43 +0800

    first commit

---
+++ src/arraySum.ts
@@ -0,0 +1,26 @@
+/**
+ * 計算數字陣列的總和
+ * @param numbers - 要計算總和的數字陣列
+ * 
+ * 範例:
+ * 輸入: [1, 2, 3, 4, 5]
+ * 輸出: 15
+ * 
+ * 輸入: [-1, -2, -3]
+ * 輸出: -6
+ * 
+ * 輸入: []
+ * 輸出: 0
+ */
+export function arraySum(numbers: number[]): number {
+    // 在此實現函式
+    let sum = 0;
+    if (numbers.length === 0) {
+        return sum;
+    } else {
+        numbers.forEach(num => {
+            sum += num;
+        });
+        return sum;
+    }
+}
